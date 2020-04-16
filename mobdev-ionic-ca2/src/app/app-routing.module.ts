6f3417b8e23590d0ef95e1e986a05cf498dc552d
vitoriareges <vitoriareges@hotmail.com>
2020-04-16 23:31:23 +0000

    atualizacao funcionando

--- mobdev-ionic-ca2/src/app/app-routing.module.ts
+++ mobdev-ionic-ca2/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+import { HttpClientModule } from '@angular/common/http'
 const routes: Routes = [
   { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)}
 ];
