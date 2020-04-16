7f3dc1ca6660e10161ae3640873636b1d469bdad
vitoriareges <vitoriareges@hotmail.com>
2020-04-16 23:47:46 +0000

    sem fazer a lista

--- mobdev-ionic-ca2/src/app/services/api.service.ts
+++ mobdev-ionic-ca2/src/app/services/api.service.ts
@@ -6,7 +6,7 @@ import {  HttpClient } from '@angular/common/http';
 })
 export class ApiService {
 
-  constructor(private http: HttpClient ) { 
+  constructor(private http: HttpClient) { 
 }
 
 getEpisodes(){
