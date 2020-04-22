4046f267994acb59e443bf8af7ac46bbe8ddee80
vitoriareges <vitoriareges@hotmail.com>
2020-04-22 20:42:14 +0000

    service

--- mobdev-ionic-ca2/src/app/services/api.service.ts
+++ mobdev-ionic-ca2/src/app/services/api.service.ts
@@ -14,12 +14,12 @@ getEpisodes(){
 }
 
 getEpisode(id){
-    return this.http.get('https://www.breakingbadapi.com/api/episodes/${id}')
+    return this.http.get(`https://www.breakingbadapi.com/api/episodes/${id}`)
 }
 getCharacters(){
     return this.http.get('https://www.breakingbadapi.com/api/characters')
 }
 getCharacter(id){
-    return this.http.get('https://www.breakingbadapi.com/api/characters/${id}')
+    return this.http.get(`https://www.breakingbadapi.com/api/characters/${id}`)
 }
 }
