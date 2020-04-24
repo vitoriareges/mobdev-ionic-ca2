759899bad3257726f6227587d822e15e5e55271e
vitoriareges <vitoriareges@hotmail.com>
2020-04-24 19:47:31 +0000

    char

--- mobdev-ionic-ca2/src/app/pages/characters/characters.page.ts
+++ mobdev-ionic-ca2/src/app/pages/characters/characters.page.ts
@@ -4,21 +4,21 @@ import { Router } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
 
 @Component({
-  selector: 'app-characters',
-  templateUrl: './characters.page.html',
-  styleUrls: ['./characters.page.scss'],
+    selector: 'app-characters',
+    templateUrl: './characters.page.html',
+    styleUrls: ['./characters.page.scss'],
 })
 export class CharactersPage implements OnInit {
-characters: Observable<any>;
-  constructor(private router: Router, private api: ApiService) { }
-  ngOnInit() {
-      this.characters = this.api.getCharacters();
-      this.characters.subscribe(data => {console.log('my data; ', data)});
-  }
+    characters: Observable<any>;
+    constructor(private router: Router, private api: ApiService) { }
+    ngOnInit() {
+        this.characters = this.api.getCharacters();
+        this.characters.subscribe(data => { console.log('my data; ', data) });
+    }
 
-  openDetails(character){
-let characterId = character.char_id;
-this.router.navigateByUrl(`/tabs/characters/${characterId}`);
-}
+    openDetails(character) {
+        let characterId = character.char_id;
+        this.router.navigateByUrl(`/tabs/characters/${characterId}`);
+    }
 
 }
