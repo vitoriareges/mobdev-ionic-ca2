c5bf076f926dd1283c2347f3b672ee0091d2fc16
vitoriareges <vitoriareges@hotmail.com>
2020-04-25 00:39:52 +0000

    implementing infinite scroll

--- mobdev-ionic-ca2/src/app/pages/quotes/quotes.page.ts
+++ mobdev-ionic-ca2/src/app/pages/quotes/quotes.page.ts
@@ -14,10 +14,28 @@ export class QuotesPage implements OnInit {
 
     
     quotes: Observable<any>;
+     quote = [];
+  numTimesLeft = 5;
 
-    constructor(private router: Router, private api: ApiService) { }
+    constructor(private router: Router, private api: ApiService) {
+        this.ngOnInit();
+     }
+
+     
+ loadData(event) {
+    setTimeout(() => {
+      console.log('Done');
+      this.ngOnInit();
+      this.numTimesLeft -= 1;
+      event.target.complete();
+    }, 2000);
+  }
+
+  
 
     ngOnInit() {
+        for (let i=0; i<10; i++){
+        this.quote.push(i);}
         this.quotes = this.api.getQuotes();
         this.quotes.subscribe(data => {
         console.log('my data: ', data);
@@ -29,4 +47,10 @@ export class QuotesPage implements OnInit {
         
         this.router.navigateByUrl('/tabs/quotes/${id}');
     }
+
+    
 }
+
+ 
+
+  
